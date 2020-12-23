ff2b72b9c34e92a4471d2988300d3387db0173d3
JC <bonassin@gmail.com>
2020-12-23 18:25:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ ef key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |key, Value|
-    if lowest_value == nil || value < lowest_value
+    if lowest_value == nil || Value < lowest_value
       lowest_value = Value
       lowest_key = key
     end
